3741bb7a9d16b4c0089623b0d67c71ebe2de2695
zhoujian <zhoujian>
2018-06-15 11:01:38 +0800

    日志采集分析

---
+++ src/main/java/com/emt/vo/LogHandle.java
@@ -0,0 +1,47 @@
+package com.emt.vo;
+
+public class LogHandle {
+	
+	private String id;
+	
+	private String serialId;
+	
+	private String startTime;
+	
+	private String endTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSerialId() {
+		return serialId;
+	}
+
+	public void setSerialId(String serialId) {
+		this.serialId = serialId;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	
+	
+	
+}
